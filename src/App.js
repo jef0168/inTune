b28aa0a14506d524cdc665fdeab26215035ade1c
Jovanny <JovannyFrias@my.unt.edu>
2019-09-17 15:18:51 -0500

    Front End UI base

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+//Pages
+import home from './pages/home';
+import signup from './pages/signup';
+import login from './pages/login';
+import explore from './pages/explore';
+import profile from './pages/profile';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Used for the path at the end of the url to navigate to the different .js files i have in the pages folder */}
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/signup" component={signup} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/explore" component={explore} />
+          <Route exact path="/profile" component={profile} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
