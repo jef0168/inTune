99a4da6d4657dc1eace74f8fc6fd06a737ca2c58
Jovanny Frias <jovanny.frias806@gmail.com>
2019-09-18 21:29:16 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+//Components
+import Navbar from './components/Navbar';
+
 //Pages
 import home from './pages/home';
 import signup from './pages/signup';
@@ -15,13 +18,16 @@ function App() {
     <div className="App">
       {/* Used for the path at the end of the url to navigate to the different .js files i have in the pages folder */}
       <Router>
-        <Switch>
-          <Route exact path="/" component={home} />
-          <Route exact path="/signup" component={signup} />
-          <Route exact path="/login" component={login} />
-          <Route exact path="/explore" component={explore} />
-          <Route exact path="/profile" component={profile} />
-        </Switch>
+      <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/signup" component={signup} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/explore" component={explore} />
+            <Route exact path="/profile" component={profile} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
