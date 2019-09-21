9827e4a7c08ac84ecc3a999eff96ad27123a4a14
Jovanny Frias <jovanny.frias806@gmail.com>
2019-09-20 20:35:51 -0500

    Add files via upload

---
+++ src/pages/login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Login} from '../components/login/index'
+
+export class login extends Component {
+    render() {
+        return (
+            <Login/>
+        )
+    }
+}
+
+export default login;
